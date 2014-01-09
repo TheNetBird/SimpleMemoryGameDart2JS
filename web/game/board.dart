b38b16a70616c61a720fcb564129e733a6d0e4c4
Erik Feucht <ErikFeucht@gmail.com>
2014-01-09 17:34:42 +0100

    Added Board functions

--- web/game/board.dart
+++ web/game/board.dart
@@ -1,5 +1,30 @@
 part of simple_memory;
 
 class Board {
+  List<List<Card>> cards = new List<List<Card>>();
+  
+  void revealAll() {
+    for (int i = 0; i < cards.length; i++) {
+      for (int j = 0; j < cards[i].length; j++) {
+        cards[i][j].revealed = true;
+      }
+    }
+  }
+  
+  void hideAll() {
+    for (int i = 0; i < cards.length; i++) {
+      for (int j = 0; j < cards[i].length; j++) {
+        cards[i][j].revealed = false;
+      }
+    }
+  }
+  
+  void draw() {
+    for (int i = 0; i < cards.length; i++) {
+      for (int j = 0; j < cards[i].length; j++) {
+        cards[i][j].draw();
+      }
+    }
+  }
   
 }
--- web/game/card.dart
+++ web/game/card.dart
@@ -2,6 +2,11 @@ part of simple_memory;
 
 
 class Card {
+  // Hold whether or not it is currently revealed
+  // Holds directions to draw itself
+  bool revealed = false;
   
-  
+  void draw() {
+    
+  }
 }
